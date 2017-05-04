8164a2ddd570f117bd7774a134291695953aed83
izardj <izard.jerome@free.fr>
2017-05-04 14:14:29 +0200

    Annotations pour un Webservice

--- src/ws/BanqueService.java
+++ src/ws/BanqueService.java
@@ -4,38 +4,46 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
 import metier.Compte;
 
+@WebService(serviceName = "BanqueWS")
 public class BanqueService {
 
-	public double conversionEuroDollar(double montant){
+	@WebMethod(operationName = "ConversionED")
+	public double conversionEuroDollar(@WebParam(name = "mt") double montant) {
 		return montant * 1.09;
 	}
-	
-	public Compte getCompte(int code){
+
+	@WebMethod
+	public Compte getCompte(@WebParam(name = "code") int code) {
 		Compte c = new Compte();
 		c.setCode(code);
 		c.setDateCreation(new Date());
 		c.setSolde(10000);
 		return c;
 	}
-	
-	public List<Compte> getComptes(){
+
+	@WebMethod
+	public List<Compte> getComptes() {
 		List<Compte> cptes = new ArrayList<Compte>();
-		
+
 		Compte c1 = new Compte();
 		c1.setCode(2);
 		c1.setDateCreation(new Date());
 		c1.setSolde(2000);
-		
+
 		Compte c2 = new Compte();
 		c2.setCode(3);
 		c2.setDateCreation(new Date());
 		c2.setSolde(3000);
-		
+
 		cptes.add(c1);
 		cptes.add(c2);
-		
+
 		return cptes;
 	}
 }
